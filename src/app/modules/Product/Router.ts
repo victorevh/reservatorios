8b9576e0b0bc1ef567b4691a9d94406accae0181
victorevh <victor_oliveirasantos@hotmail.com>
2022-12-28 16:46:43 -0300

    Add filter EAR metric

--- src/app/modules/Product/Router.ts
+++ src/app/modules/Product/Router.ts
@@ -1,27 +1,57 @@
-import { Router, request, response, Request, Response } from 'express';
+import { Router, request, response, Request, Response } from "express";
+import { EARmetricsType } from "@src/services/type";
 
 const router = Router();
-const axios = require('axios');
+const axios = require("axios");
 
+router.get("/", (req: Request, res: Response) => {
+  const axios = require("axios");
 
-router.get('/', (req: Request, res: Response) => {
-    const axios = require('axios');
+  (async () => {
+    try {
+      const response = await axios.get(
+        "http://tr.ons.org.br/Content/Get/SituacaoDosReservatorios"
+      );
+      const resData = response.data;
 
-    (async () => {
-      try {
-        const response = await axios.get('http://tr.ons.org.br/Content/Get/SituacaoDosReservatorios')
-        const resData = response.data
+      const EARNorteData = resData[0].SubsistemaValorUtil;
+      const EARNortestringify = JSON.stringify(EARNorteData);
+      const EARNorte = EARNortestringify.slice(0,5)
+      
+      const EARNordesteData = resData[3].SubsistemaValorUtil;
+      const EARNordestestringify = JSON.stringify(EARNordesteData);
+      const EARNordeste = EARNordestestringify.slice(0,5)
 
-        return res.status(200).send({
-            resData
-        })
-    
-      } catch (error) {
-        console.log(error);
-        return res.status(400).send({ error });
-      }
-    })();
-    
+      const EARSulData = resData[7].SubsistemaValorUtil;
+      const EARSulstringify = JSON.stringify(EARSulData);
+      const EARSul = EARSulstringify.slice(0,5)
+
+      const EARSudesteCentroOesteData = resData[19].SubsistemaValorUtil;
+      const EARSudesteCentroOestestringify = JSON.stringify(EARSudesteCentroOesteData);
+      const EARSudesteCentroOeste= EARSudesteCentroOestestringify.slice(0,5)
+
+      const metricsEAR: EARmetricsType = {
+        "Sudeste / Centro-Oeste": EARSudesteCentroOeste,
+        "Sul": EARSul,
+        "Nordeste": EARNordeste,
+        "Norte": EARNorte,
+      };
+
+      const nordeste = resData.filter(
+        (item: any) => item.Subsistema === "Nordeste"
+      );
+      const nordesteReservatórios = nordeste.map((item: any) => {
+        return item.Bacia;
+      });
+
+      return res.status(200).send({
+        metricsEAR,
+      });
+    } catch (error) {
+      console.log(error);
+      return res.status(400).send({ error });
+    }
+  })();
 });
 
 export default router;
