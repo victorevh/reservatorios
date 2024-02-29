53f1fdfd54b7a90f736086552146f0f4b1fc50b8
Victor Oliveira Santos <victor_oliveirasantos@hotmail.com>
2024-02-28 22:15:08 -0300

    Configura schedular

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,11 @@
-import * as functions from 'firebase-functions';
-import {updateStaticReservations} from './functions/updateReservations';
+import * as functions from "firebase-functions";
+import { updateStaticReservations } from "./functions/updateReservations";
 
-
-export const getReservationsRun = functions
+export const getReservationRun = functions
   .runWith({
     timeoutSeconds: 540,
-    memory: '2GB',
+    memory: "2GB",
   })
-  .pubsub.schedule('0 9,17 * * *') // Diariamente às 9 da manhã e 17 da tarde.
-  .timeZone('America/Sao_Paulo')
-  .onRun(async () => await updateStaticReservations())
-
-export const getReservations = functions
-  .runWith({
-    timeoutSeconds: 540,
-    memory: '2GB',
-  })
-  .https.onRequest(async (req, res) => {
-    await updateStaticReservations();
-    res.status(200).send('Dados atualizados com sucesso!');
-  });
+  .pubsub.schedule("0 14 * * *") // Diariamente às 14 da tarde.
+  .timeZone("America/Sao_Paulo")
+  .onRun(async () => await updateStaticReservations());
